af226072c5f5993bede7865da84f8d5be57e09e4
shkim <shkim@pmirnc.com>
2021-02-16 11:32:44 +0900

    Asset Image & Data Load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +10,15 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add teh image to our existing div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,18 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-    ]
-  }
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
 };
